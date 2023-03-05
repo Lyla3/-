a308f882d98fd758dd96f5429472d22994b5b3f6
Sumin Lee <125568161+Lyla3@users.noreply.github.com>
2023-03-05 21:44:01 +0900

    Create 중첩타입.swift

---
+++ 문법/중첩타입.swift
@@ -0,0 +1,14 @@
+//키워드 저장
+
+struct DataCore {
+  
+    struct satisfactionString {
+        static let sat60 = "~60%"
+        static let sat75 = "75%"
+        static let sat100 = "100%"
+        static let sat110 = "110%"
+        static let satOver = "과식"
+    }
+    
+}
+
