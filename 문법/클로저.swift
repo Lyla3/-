34ec2214c52aacf6a9b8658ceace7a90d9494c6c
Sumin Lee <125568161+Lyla3@users.noreply.github.com>
2023-03-06 20:18:00 +0900

    Create 클로저.swift

---
+++ 문법/클로저.swift
@@ -0,0 +1,18 @@
+func closureCaseFunction(a: Int,b : Int, clousure: (Int) -> Void) {
+    let c = a + b
+    clousure(c)
+}
+
+func doSomething (_ a:Int) -> Void {
+    print("안녕: \(a)")
+}
+
+//(Int) -> Void형 타입 객체
+let functionB: (Int) -> Void
+
+functionB = doSomething(_:)
+
+closureCaseFunction(a:10,b: 20, clousure: functionB)
+closureCaseFunction(a: 20, b: 20) {n in
+    print("클로저 실행\(n)")
+}
