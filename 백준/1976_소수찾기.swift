570cf36ef2949eb42397d0b86ddfb4e66fec37ca
Sumin Lee <125568161+Lyla3@users.noreply.github.com>
2023-03-08 23:43:45 +0900

    Update and rename 1976_소수찾기 to 1976_소수찾기.swift
    isPrime 속성 이용, for문 종료 후에 추가되도록 함

--- 백준/1976_소수찾기.swift
+++ 백준/1976_소수찾기.swift
@@ -8,12 +8,19 @@ for i in numberArray {
     if i == 1 {
         continue
     } else {
+        if i == 2 {countNum += 1}
+        else{
+        var isPrime = true
         for l in 2...(i-1) {
             if i % l == 0 {
+                isPrime = false
                 break
             }
         }
-        countNum += 1
+            if isPrime {
+                countNum += 1
+            }
+        }
     }
 }
 
