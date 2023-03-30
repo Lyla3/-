63f70ea067078406147f55e72bef49c83f3cc27b
Sumin Lee <125568161+Lyla3@users.noreply.github.com>
2023-03-30 23:35:10 +0900

    Update 제네릭.swift
    제네릭 타입 제약 설정

--- 문법/제네릭.swift
+++ 문법/제네릭.swift
@@ -1,6 +1,3 @@
-
-
-
 func resultType(m: Int) -> Result<Bool, valueError> {
     if m > 5 {
     	return Result.failure(valueError.maxError)
@@ -11,4 +8,10 @@ func resultType(m: Int) -> Result<Bool, valueError> {
 
 func resultType(m: Int) -> Result<Success, Failure> {
     // Bool,   Error 타입, 정의된 Success, Failure에 *실제 타입*을 넣어서 쓴다. 
+}
+
+
+//타입에 제약 설정 (equatable)
+func isEqualValues<T:equatable>(_ a: T, _ b: T) -> Bool {
+    return a == b               
 }
