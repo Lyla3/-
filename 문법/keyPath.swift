319b48c1604b5b49410e85391669d358d585a382
Sumin Lee <125568161+Lyla3@users.noreply.github.com>
2023-04-16 20:02:05 +0900

    Create keyPath.swift

---
+++ 문법/keyPath.swift
@@ -0,0 +1,22 @@
+class Dog {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+
+
+let dog1 = Dog(name: "초코")
+
+dog1.name
+
+"dog1.name"    // 이런식으로 접근하면 안될까?
+
+
+
+
+
+let namePath = \School.affiliate.classMember.name      // 미리 경로를 지정 (keyPath)
+
+school1[keyPath: namePath] 
